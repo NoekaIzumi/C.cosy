e9cefe61e68fed0a2aace3a748dd61370217e930
NoekaIzumi <noeka-izumi@outlook.jp>
2024-01-02 16:14:19 +0000

    [Canged PostTags_columm_t.bigint]

--- db/migrate/20231127113251_create_post_tags.rb
+++ db/migrate/20231127113251_create_post_tags.rb
@@ -2,8 +2,8 @@ class CreatePostTags < ActiveRecord::Migration[7.0]
   def change
     create_table :post_tags do |t|
 
-      t.references :post, foreign_key: true,   null: false
-      t.references :tag,  foreign_key: true,   null: false
+       t.bigint :post, foreign_key: true,   null: false
+       t.bigint :tag,  foreign_key: true,   null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_02_152016) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_02_152016) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -60,8 +60,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_02_152016) do
   end
 
   create_table "favorites", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "post_id"
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_favorites_on_post_id"
@@ -69,12 +69,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_02_152016) do
   end
 
   create_table "post_tags", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "tag_id", null: false
+    t.bigint "post", null: false
+    t.bigint "tag", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_post_tags_on_post_id"
-    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -121,8 +119,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_02_152016) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "favorites", "posts"
   add_foreign_key "favorites", "users"
-  add_foreign_key "post_tags", "posts"
-  add_foreign_key "post_tags", "tags"
   add_foreign_key "posts", "users"
   add_foreign_key "relationships", "users", column: "follower_id"
   add_foreign_key "relationships", "users", column: "followered_id"
